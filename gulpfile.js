9a18325fdd086ec38fa313261abaddf13a7a78b7
Henrik Backman <henrik.backman@hm.com>
2017-01-03 10:44:08 +0100

    Adding config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,47 @@
 const gulp = require('gulp'),
+      gutil = require('gulp-util'),
       sass = require('gulp-sass'),
       concat = require('gulp-concat'),
       depcheck = require('depcheck'),
       checkDeps = require('gulp-check-deps'),
       path = require('path'),
       childProcess = require('child_process'),
-      runSequence = require('run-sequence');
+      fork = require('child_process').fork,
+      runSequence = require('run-sequence'),
+      async = require('async'),
+      config = require('./app/config')();
+
+const app = {
+  instance: null,
+  path: 'app.js',
+  env: process.env,
+  start: function(callback) {
+    app.instance = fork(app.path, {
+      silent: true, env: app.env
+    });
+
+    if (app.instance) {
+      childProcess.exec('npm start');
+      gutil.log(gutil.colors.cyan('Starting'), 'express server listening on port', config.port);
+      // app.instance.stdout.pipe(process.stdout);
+      // app.instance.stderr.pipe(process.stderr);
+    }
+    callback();
+  },
+  stop: function(callback) {
+    if (app.instance) {
+      gutil.log(gutil.colors.red('Stopping'), 'express server ( PID:', app.instance.pid, ')');
+      app.instance.kill('SIGTERM');
+    }
+    callback();
+  },
+  restart: function() {
+    async.series([
+      app.stop,
+      app.start
+    ]);
+  },
+};
 
 const assets = {
   sass: 'app/assets/sass/app.scss',
@@ -14,6 +50,8 @@ const assets = {
     vendor: '',
   },
   images: 'app/assets/img/**/*',
+  jade: 'app/views/**/*.jade',
+  node: 'routes/**/*',
 };
 
 // Clean the /public directory
@@ -106,6 +144,7 @@ gulp.task('watch', function () {
   gulp.watch(assets.sass, ['sass']);
   gulp.watch(assets.scripts.vendor, ['scripts']);
   gulp.watch(assets.scripts.app, ['scripts']);
+  gulp.watch(assets.node, app.restart);
 });
 
 gulp.task('build', function(callback) {
@@ -119,8 +158,10 @@ gulp.task('build', function(callback) {
     callback);
 });
 
-gulp.task('start-service', function () {
-  childProcess.exec('npm start');
+gulp.task('start-service', app.start);
+
+gulp.task('exit', function () {
+  childProcess.exec('^C');
 });
 
 gulp.task('start', function(callback) {
