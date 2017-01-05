9227939abbe739d5feddd6f9565f341d580ac08a
Henrik Backman <henrik.backman@hm.com>
2017-01-05 22:27:38 +0100

    Add New Relic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['henrikbackman'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'fc9333a291469a7ebca2896d5b689964400bdb77',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
